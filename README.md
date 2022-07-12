9fa9967e2269ad32e1a5725ecd011a59f413c754
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2022-07-13 00:47:13 +0530

    contract: add constructor and method
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
-# Sample Hardhat Project
+# Ballot Smart Contract 
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
-
-Try running some of the following tasks:
 
 ```shell
-npx hardhat help
-npx hardhat test
-GAS_REPORT=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.js
+yarn init
+yarn add --dev hardhat
+yarn hardhat
+yarn hardhat run scripts/ballot_deploy.js
+yarn hardhat clean
+
 ```
--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -18,7 +18,29 @@ contract BallotContract{
     }
 
     mapping (address=>Voter) public voters;
-    Proposal[] public proposal;
+    Proposal[] public proposals;
+
+
+    constructor(bytes32[] memory c_proposal){
+
+        admin = msg.sender;
+        voters[msg.sender].weight =1;
+        for(uint64 i=0;i<c_proposal.length;i++)
+        {
+            proposals.push(Proposal({ name: c_proposal[i],voteCount: 0 }));
+        }
+
+    }
+
+    function giveAcessToVote(address voter_t)  external{
+
+        require(msg.sender==admin,"you dont have access");
+        require(voters[voter_t].isVoted == false,"Already voted");
+        require(voters[voter_t].weight==0,"weight not zero wrong call");
+        
+        voters[voter_t].weight =1;
+
+    }    
 
 
 
