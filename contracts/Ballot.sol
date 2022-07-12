f231f1ca45c2597c95c791ea6f7d16be5c8325bb
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2022-07-13 00:14:50 +0530

    data to be stored
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -2,8 +2,28 @@
 pragma solidity ^0.8.0;
 
 contract BallotContract{
+
+    address public admin;
+
+    struct Proposal {
+        bytes32 name;
+        uint256 voteCount;
+    } 
+
+    struct Voter{
+        address delegate;//A person who represents others.
+        uint256 weight;
+        bool isVoted;
+        uint256 votedTo;
+    }
+
+    mapping (address=>Voter) public voters;
+    Proposal[] public proposal;
+
+
+
     
-    
+
 
 
 
